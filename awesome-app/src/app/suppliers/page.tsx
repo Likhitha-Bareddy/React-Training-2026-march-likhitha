549032da4f9440ccffdd1bcf6815be596431ed11
Likhitha Bareddy <likitha523@gmail.com>
2026-03-26 21:42:32 -0700

    change 9

--- awesome-app/src/app/suppliers/page.tsx
+++ awesome-app/src/app/suppliers/page.tsx
@@ -2,48 +2,52 @@
 import { Supplier } from "@/models/Supplier";
 import { useEffect, useState } from "react";
 
-export default function SuppliersPage(){
+export default function SuppliersPage() {
 
     const [query, setQuery] = useState("");
     const [suppliers, setSuppliers] = useState<Supplier[]>([]);
 
-    async function fetchSuppliers(query?: string){
+    async function fetchSuppliers(query?: string) {
         const response = await fetch("http://localhost:3000/api/suppliers?q=" + query);
         const suppliers = await response.json() as Supplier[];
         setSuppliers(suppliers);
     }
 
     useEffect(() => {
-    fetchSuppliers("");
-}, []);
-    // const suppliers = await fetchSuppliers();
-    // console.log("Suppliers are: ", suppliers);
+        fetchSuppliers("");
+    }, []);
 
-    // const response = await fetch("http://localhost:3000/api/suppliers?q=");
-    // // const suppliers2 = await response.json() as Supplier[];
-    //  setSuppliers(await response.json() as Supplier[]);
- 
-    async function handleSearch(){
+
+    async function handleSearch() {
         await fetchSuppliers(query);
+    }
+
+    async function handleReset() {
+        setQuery("");
+        fetchSuppliers("");
     }
 
     return (
         <div>
             <h4>Supplier Listings</h4>
-             
-            <div>
-                <input
-          type="text"
-          placeholder="Search for..."
-          onChange={evt => setQuery(evt.target.value)}
-          value={query} // The input value is controlled by React state
-          // The state is updated on each change
-        />
-         <button className="btn btn-primary"  onClick={handleSearch} >Search</button>
-            
-            </div> 
 
-             <table className="table">
+            <div>
+                <label htmlFor={query}>
+        <b>Enter Search Term:</b>
+      </label>&nbsp;
+                <input style={{width: '500px'}} id="cs-query"
+                    type="text"
+                    placeholder="Search for..."
+                    onChange={evt => setQuery(evt.target.value)}
+                    value={query} // The input value is controlled by React state
+                // The state is updated on each change
+                />&nbsp;
+                <button className="btn btn-primary" onClick={handleSearch} >Search</button>&nbsp;
+                <button className="btn btn-secondary" onClick={handleReset}>Reset</button>
+
+            </div>
+
+            <table className="table">
                 <thead>
                     <tr>
                         <th>ID</th>
