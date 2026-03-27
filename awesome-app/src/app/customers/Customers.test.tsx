78d7bd433d7421d995beaa2c51a30362657c1cc5
Likhitha Bareddy <likitha523@gmail.com>
2026-03-27 04:58:09 -0700

    change 10 day 5 latest

---
+++ awesome-app/src/app/customers/Customers.test.tsx
@@ -0,0 +1,25 @@
+import { CustomerPage } from "./page";
+import { render, screen } from "@testing-library/react";
+
+//Mock the global fetch function
+beforeEach(() => {
+    fetchMock.resetMocks();
+});
+
+test("render customers", async () => {
+
+    const mockCustomers = [
+        {id: 1, name: "Customer 1", location: "Mumbai"},
+        {id: 2, name: "Customer 2", location: "Bangalore"},
+    ];
+
+    fetchMock.mockResponseOnce(JSON.stringify(mockCustomers));
+
+    render(await CustomerPage({timeout: 1000}));
+
+    const customer1 = await screen.findByText("Customer 1");
+    expect(customer1).toBeInTheDocument();
+
+
+
+})
